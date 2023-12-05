6aa86bd863ec42913a5cc66afffef346a356d2a5
ИС201-1 <IS201-1@tisbi.ru>
2023-12-05 16:17:10 +0300

    j

---
+++ DirectorWorkBakaev/DirectorWorkBakaev/AuthorizationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectorWorkBakaev
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationFrame.Navigate(new RegistrationPage());
+            LogoLable.Visibility = Visibility.Hidden;
+        }
+        private void AuthorizationButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationFrame.Navigate(new ListPage());
+            LogoLable.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ DirectorWorkBakaev/DirectorWorkBakaev/RegistrationPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DirectorWorkBakaev
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationPage.xaml
+    /// </summary>
+    public partial class RegistrationPage : Page
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Аккаунт зарегистрирован!");
+            RegistrationButton.Navigate(new AuthorizationPage());
+        }
+
+        private void LogBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox lb = (TextBox)sender;
+            lb.Text = string.Empty;
+            lb.GotFocus -= LogBox_GotFocus;
+        }
+
+        private void AgeBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox ab = (TextBox)sender;
+            ab.Text = string.Empty;
+            ab.GotFocus -= AgeBox_GotFocus;
+        }
+
+        private void PassBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox pb = (TextBox)sender;
+            pb.Text = string.Empty;
+            pb.GotFocus -= PassBox_GotFocus;
+        }
+
+        private void MailBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox mb = (TextBox)sender;
+            mb.Text = string.Empty;
+            mb.GotFocus -= MailBox_GotFocus;
+        }
+        private void AvaBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox mb = (TextBox)sender;
+            mb.Text = string.Empty;
+            mb.GotFocus -= AvaBox_GotFocus;
+        }
+    }
+}
